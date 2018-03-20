297863491e87f00fea3488af831903755563234e
Yasuo Ashina <ashipp3000@gmail.com>
2018-03-20 22:58:57 +0900

    9章 アニメーション

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import CssBaseline from 'material-ui/CssBaseline';
+import Reboot from 'material-ui/Reboot';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import Input from 'material-ui/Input';
 import List, { ListItem, ListItemText } from 'material-ui/List';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import './TodoApp.css';
 
 export default function TodoApp({ task, tasks, inputTask, addTask }) {
   return (
     <div>
-      <CssBaseline />
+      <Reboot />
       <AppBar position="static">
         <Toolbar>
           <Typography type="title" color="inherit">
@@ -21,17 +23,18 @@ export default function TodoApp({ task, tasks, inputTask, addTask }) {
       <div style={{ padding: '16px' }}>
         <Input onChange={(e) => inputTask(e.target.value)} />
         <Button raised color="secondary" onClick={() => addTask(task)}>add</Button>
-        {console.log(tasks)}
         <List>
-          {
-            tasks.map(function(item, i) {
-              return (
-                <ListItem key={i}>
-                  <ListItemText primary={`・${item}`} />
-                </ListItem>
-              );
-            })
-          }
+          <ReactCSSTransitionGroup transitionName="example" transitionEnterTimeout={300}>
+            {
+              tasks.map(function(item, i) {
+                return (
+                  <ListItem key={i}>
+                    <ListItemText primary={`・${item}`} />
+                  </ListItem>
+                );
+              })
+            }
+          </ReactCSSTransitionGroup>
         </List>
       </div>
     </div>
